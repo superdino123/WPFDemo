19cfa1593c13a9d802223a932fac39d48a9e97e0
diplant <1119674493@qq.com>
2017-07-18 11:41:10 +0800

    登录页面完成

--- WPFDemo/View/Login.xaml.cs
+++ WPFDemo/View/Login.xaml.cs
@@ -22,6 +22,64 @@ namespace WPFDemo.View
         public Login()
         {
             InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;//设置界面显示在屏幕中心
+        }
+
+        #region 页面逻辑
+        /// <summary>
+        /// 窗口关闭按钮点击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        #region 前端界面设计
+
+        /// <summary>
+        /// 鼠标点击窗口时窗口头部背景变色
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HeaderGrid.Background = new SolidColorBrush((Color) ColorConverter.ConvertFromString("#FB8633"));
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        /// <summary>
+        /// 窗口变成后台窗口时窗口头部背景变色
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            HeaderGrid.Background = new SolidColorBrush((Color) ColorConverter.ConvertFromString("#808080"));
+        }
+
+
+        #endregion
+
+        /// <summary>
+        /// 快捷键设置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Ctrl+F4 关闭窗口
+            if ((e.KeyboardDevice.IsKeyDown(key: Key.LeftCtrl) || e.KeyboardDevice.IsKeyDown(key: Key.RightCtrl)) &&
+                e.KeyboardDevice.IsKeyDown(key: Key.F4))
+            {
+                this.Close();
+            }
         }
     }
 }
