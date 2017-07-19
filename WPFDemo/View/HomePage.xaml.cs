b1ed663e2d512c0b70e723aa97b0b58105afd748
diplant <1119674493@qq.com>
2017-07-19 20:27:07 +0800

    编写首页左侧菜单栏，头部菜单

--- WPFDemo/View/HomePage.xaml.cs
+++ WPFDemo/View/HomePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Fluent;
+using WPFDemo.ViewModel;
 
 namespace WPFDemo.View
 {
@@ -20,9 +21,10 @@ namespace WPFDemo.View
     /// </summary>
     public partial class HomePage : RibbonWindow
     {
+
         public HomePage()
         {
-            InitializeComponent();
+            InitializeComponent();;
         }
     }
 }
---
+++ WPFDemo/View/UserControl/SystemToolsUc.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDemo.View.UserControl
+{
+    /// <summary>
+    /// SystemTools.xaml 的交互逻辑
+    /// </summary>
+    public partial class SystemToolsUc : System.Windows.Controls.UserControl
+    {
+        public SystemToolsUc()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculatorTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //"calc.exe"为计算器
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo {FileName = "calc.exe"};        
+            System.Diagnostics.Process.Start(info);
+        }
+
+        private void NotepadTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //"notepad.exe"为记事本
+            System.Diagnostics.ProcessStartInfo info = new System.Diagnostics.ProcessStartInfo { FileName = "notepad.exe" };
+            System.Diagnostics.Process.Start(info);
+        }
+
+        private void JournalTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string path = Directory.GetCurrentDirectory();
+            Process.Start(path);
+        }
+        
+    }
+}
